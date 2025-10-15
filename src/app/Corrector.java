d07553b97ee3f5a0c833115155dd972137692de9
Nico <dfp098@gmail.com>
2025-10-16 01:19:55 +0300

    Final app

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,4 +1,21 @@
 package app;
 
 public class Corrector {
-}
+
+    public String handleData(String[] strs) {
+        StringBuilder sB = new StringBuilder();
+        int count = 0;
+        for (String str : strs ){
+            count++;
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+
+            }
+            sB.append(count)
+                        .append(") ")
+                        .append(str)
+                        .append(System.lineSeparator());
+        }
+            return (sB.toString());
+        }
+    }
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,10 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+        getOutput(new Corrector().handleData(new DataProvider().getData()));
+    }
+    private static void getOutput(String output){
+        System.out.println(output);
+    }
 }
